94fdad271da6d515708e35bdf2f7f5bc7d0d3837
sarahheacock <seheacock@bellsouth.net>
2017-06-04 12:28:42 -0400

    about to deploy

--- models.js
+++ models.js
@@ -8,12 +8,26 @@ var Schema = mongoose.Schema;
 const temp = new Date().toString().split(' ');
 const NOW = new Date(temp[0] + " " + temp[1] + " " + temp[2] + " " + temp[3] + " 10:00:00").getTime();
 
+var sortUpcoming = function(a, b){
+  //negative if a before b
+  //0 if unchanged order
+  //position if a after b
+  return b.arrive - a.arrive;
+};
+
 var UpcomingSchema = new Schema({
   arrive: Number,
   depart: Number,
   guests: Number,
   room: String,
+  createdAt: {type:Date, default:Date.now},
 });
+
+UpcomingSchema.method("update", function(updates, callback){
+  Object.assign(this, updates, {updatedAt: new Date()});
+  this.parent().save(callback);
+});
+
 
 var UserSchema = new Schema({
   email: String,
@@ -30,6 +44,7 @@ var UserSchema = new Schema({
 });
 
 UserSchema.pre("save", function(next){
+  this.upcoming.sort(sortUpcoming);
   next();
 });
 
--- routes.js
+++ routes.js
@@ -2,29 +2,25 @@
 
 var express = require("express");
 var router = express.Router();
-
 var User = require("./models").User;
 
-var RoomList = require('./data/roomList');
-
-
 router.param("userID", function(req, res, next, id){
-  User.findById(id, function(err, doc){
+  //User.findById(id, function(err, doc){
+  User.findOne({email: id}).exec(function(err, doc){
     if(err) return next(err);
     if(!doc){
       err = new Error("Not Found");
       err.status = 404;
       return next(err);
     }
-
     req.user = doc;
     return next();
   });
 });
 
 router.param("upcomingID", function(req,res,next,id){
-  req.answer = req.user.upcoming.id(id);
-  if(!req.answer){
+  req.upcoming = req.user.upcoming.id(id);
+  if(!req.upcoming){
     err = new Error("Not Found");
     err.status = 404;
     return next(err);
@@ -32,14 +28,19 @@ router.param("upcomingID", function(req,res,next,id){
   next();
 });
 
+//============================================================
+
+//get all users
 router.get("/", function(req, res){
   User.find({})
+    .sort({createdAt: -1})
     .exec(function(err, user){
       if(err) return next(err);
       res.json(user);
     });
 });
 
+//create new user
 router.post("/", function(req, res, next){
   var user = new User(req.body);
   user.save(function(err, user){
@@ -49,9 +50,40 @@ router.post("/", function(req, res, next){
   });
 });
 
+//get user information
 router.get("/:userID", function(req, res, next){
   res.json(req.user);
 });
+
+//make new reservation
+router.post("/:userID/upcoming", function(req, res, next){
+  req.user.upcoming.push(req.body);
+  req.user.save(function(err, user){
+    if(err) return next(err);
+    res.status(201);
+    res.json(user);
+  });
+});
+
+// //update user
+// router.put("/:userID/", function(req, res){
+//   req.upcoming.update(req.body, function(err,result){
+//     if(err) return next(err);
+//     res.json(result);
+//   });
+// });
+
+//cancel reservation
+router.delete("/:userID/upcoming/:upcomingID", function(req, res){
+  req.upcoming.remove(function(err){
+    req.user.save(function(err, user){
+      if(err) return next(err);
+      res.json(user);
+    });
+  });
+});
+
+
 
 
 module.exports = router;
